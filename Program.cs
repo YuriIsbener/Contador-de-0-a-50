5211b7734720e8f57726d73715aabc70be80cf87
Yuri Isbener <yurisuhett@gmail.com>
2020-11-17 16:57:55 -0300

    Atividade Feita

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Contador_de_0_a_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conte comigo");
+
+            for(var contador = 0; contador <= 50; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+        }
+    }
+}
